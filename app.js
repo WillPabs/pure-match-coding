9e2021b02aa04659a0c5dd80d3bc340c3a4629f7
Will Pabs <pabiterowilliam@yahoo.com>
2022-11-28 17:28:54 -0500

    add modules to router

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// const registerRouter = require('./routes/register');
+// const loginRouter = require('./routes/login');
+// const createPostRouter = require('./routes/createPost');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/register', registerRouter);
+// app.use('/login', loginRouter);
+// app.use('/createPost', createPostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
