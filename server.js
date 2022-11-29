80a8b3efdb8fd16300a43ad3b84530e2d01cb2a3
Will Pabs <pabiterowilliam@yahoo.com>
2022-11-29 15:11:55 -0500

    add files for db config

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+const userController = require('./controller/user.controller')
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/api/users', (req, res) => {
+    taskController.getTasks().then(data => res.json(data));
+});
+
+app.post('/api/user', (req, res) => {
+    console.log(req.body);
+    taskController.createTask(req.body.task).then(data => res.json(data));
+});
