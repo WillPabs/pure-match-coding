7c1de993eeaba9e2a75190ac5a688b0ed6e564cd
Will <pabiterowilliam@yahoo.com>
2022-11-30 03:29:41 -0500

    fix logic for get users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const UserRepository = require('../repository/user');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.get('/users', async (req, res) => {
+  const data = await UserRepository.getUsers();
+  console.log(`${JSON.stringify(data)}`);
+  res.send(`${JSON.stringify(data)}`);
+});
+
+router.put('/user', async (req, res) => {
+  console.log(req.body);
+  await UserRepository.createUser(req.body.user).then(data => res.json(data));
 });
 
 module.exports = router;
