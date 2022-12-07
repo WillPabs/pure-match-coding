bb1d66c0f3128139a29446eb728e2f1b24aff489
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-07 15:27:47 -0500

    refactor

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,7 @@ var router = express.Router();
 const user_controller = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-  res.send('Users listing');
-});
-
-router.get('/users', user_controller.user_list);
+router.get('/', user_controller.user_list);
 
 router.post('/user', user_controller.user_create_post);
 
