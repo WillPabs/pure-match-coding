db1f04240a9519d2d49ba23f71af57fe112b03d5
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-09 15:44:26 -0500

    add route for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const authJWT = require('../auth/authJWT');
+
+const post_controller = require('../controllers/postController');
 
 /* GET posts page. */
-router.get('/', function(req, res, next) {
-  res.render('posts', { title: 'Posts' });
-});
+router.get('/', authJWT.verifyJWT, post_controller.user_posts_list);
+
+router.post('/', authJWT.verifyJWT, post_controller.post_create);
 
 module.exports = router;
