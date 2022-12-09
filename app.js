b2c4fe838cd2036e4f056b6fdef453f91a7ad150
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-09 13:34:59 -0500

    remove unused modules

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const registerRouter = require('./routes/register');
-const loginRouter = require('./routes/login');
-const createPostRouter = require('./routes/createPost');
 const postsRouter = require('./routes/posts');
 
 var app = express();
@@ -25,9 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
-app.use('/createPost', createPostRouter);
 app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
--- routes/createPost.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET createPost page. */
-router.get('/', function(req, res, next) {
-  res.render('createPost', { title: 'Create A Post' });
-});
-
-module.exports = router;
