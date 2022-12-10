ce9309b40d2fb6d2bd1c359d231e272daa80f57e
Will <pabiterowilliam@yahoo.com>
2022-12-09 20:53:03 -0500

    add condition when posts doesn't exist

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,17 @@
 const PostRepository = require('../repository/post');
 
 exports.user_posts_list = async (req, res) => {
-    const { user } = req;
-    console.log(user.id);
-    const posts = await PostRepository.getPostsByUser(user.id);
-    res.status(200).json(posts);
+    try {
+        const { user } = req;
+        console.log("id::::",user);
+        const posts = await PostRepository.getPostsByUser(user.id);
+        if (!posts) {
+            return res.send({message: 'No posts.'});
+        }
+        return res.status(200).json(posts);
+    } catch (e) {
+        res.send({ message: e });
+    }
 };
 
 exports.post_create = async (req, res) => {
