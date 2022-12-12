043e9d8e564afdd5876c919f972d957f455e62ab
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-12 15:23:06 -0500

    add function to get all posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,11 +3,12 @@ const PostRepository = require('../repository/post');
 exports.user_posts_list = async (req, res) => {
     try {
         const { user } = req;
-        console.log("id::::",user);
+        console.log("id::::",req.user.id);
+        const userId = req.user.id;
         if (user === null || user === undefined) {
             return res.status(403).send({ message: 'Please Login' });
         }
-        const posts = await PostRepository.getPostsByUser(user.id);
+        const posts = await PostRepository.getPostsByUser(userId);
         if (!posts) {
             return res.send({message: 'No posts.'});
         }
@@ -19,11 +20,18 @@ exports.user_posts_list = async (req, res) => {
 
 exports.post_create = async (req, res) => {
     try {
-        const { post } = req;
-        const newPost = await PostRepository.createPost(post);
-        res.status(201).json(newPost);
+        const { post } = req.body;
+        const { id } = req.user;
+        console.log("******USER",req)
+        const newPost = await PostRepository.createPost(post, id);
+        return res.status(201).json(newPost);
     } catch (e) {
         console.log(e);
-        res.send({ message: e});
+        return res.send({ message: e });
     }
+}
+
+exports.all_users_posts = async (req, res) => {
+    const posts = await PostRepository.getAllPosts();
+    return res.json(posts);
 }
