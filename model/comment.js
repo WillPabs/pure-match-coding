053e250e1fc0ab0614503d61d2187dc47a9ed653
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-13 16:41:20 -0500

    add comment model

---
+++ model/comment.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes, Model) => {
+    const Comments = sequelize.define('comment', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            allowNull: false,
+            unique: true
+        },
+        postId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: { 
+                model: 'posts',
+                key: 'id',
+            },
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: { 
+                model: 'users',
+                key: 'id',
+            },
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        }
+    }, {
+        sequelize,
+        modelName: 'comments',
+        timestamps: false
+    })
+
+    return Comments;
+}
