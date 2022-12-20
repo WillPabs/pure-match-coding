f60d245196d2962b3674c3f1adaa0dcc86c096c3
Will Pabs <pabiterowilliam@yahoo.com>
2022-12-20 13:57:55 -0500

    link register and login routes to frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Pure Match',
+    loginText: 'Login',
+    loginRoute: '/login',
+    registerText: 'Register',
+    registerRoute:  '/register',
+  });
 });
 
 module.exports = router;
